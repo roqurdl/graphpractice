19bde26a3720a5fddd5427aa6caa9c85eeb66273
roqurdl <uyhnri@gmail.com>
2022-10-05 22:03:22 +0900

    typeDefs and resolvers

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ References Twitter API
 
 Write it mandatory
 Define type => Like Schema in mongodb
-query => build a url Like REST API
+type query => build a url Like REST API Get request = give Data to user
+=>DB에 영향 X
+type mutation => Post request = user send Data(Delete,Update,Create)
+굳이 나누지 않아도 작동은 함. But 의도의 명확성의 문제
+type에 !를 붙이면 null을 허용하지 않음 required 표시
--- server.js
+++ server.js
@@ -1,21 +1,66 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+  {
+    id: "1",
+    text: "first one!",
+  },
+  {
+    id: "2",
+    text: "second one",
+  },
+];
+
 const typeDefs = gql`
   type User {
-    id: ID
-    username: String
+    id: ID!
+    firstname: String!
+    lastname: String!
   }
   type Tweet {
-    id: ID
-    text: String
+    id: ID!
+    text: String!
     author: User
   }
   type Query {
-    allTweets: [Tweet]
-    tweet(id: ID): Tweet
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    ping: String!
+  }
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
-const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(_, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+  Mutation: {
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+      };
+      tweets.push(newTweet);
+      return newTweet;
+    },
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
+      return true;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
